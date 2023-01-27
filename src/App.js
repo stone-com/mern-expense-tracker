a98b9229478389f2a5fac6dce202d5d592c77dbe
Stone Comstock <comstockstone@yahoo.com>
2023-01-27 15:13:35 +0700

    More basic css styling on transaction section

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       </h1>
       <form>
         <div className='basic'>
-          <input type='text' placeholder={'New Purchase'} />
+          <input type='text' placeholder={'New Transaction'} />
           <input type='datetime-local' />
         </div>
         <div className='description'>
@@ -23,27 +23,27 @@ function App() {
             <div className="description">It was time for a new TV</div>
           </div>
           <div className="right">
-            <div className="price">$500</div>
+            <div className="price red">-$500</div>
             <div className="datetime">2022-12-18 13:45</div>
           </div>
         </div>
         <div className="transaction">
           <div className="left">
-            <div className="name">New Samsung TV</div>
+            <div className="name">Got Paid</div>
             <div className="description">It was time for a new TV</div>
           </div>
           <div className="right">
-            <div className="price">$500</div>
+            <div className="price green">+$1000</div>
             <div className="datetime">2022-12-18 13:45</div>
           </div>
         </div>
         <div className="transaction">
           <div className="left">
-            <div className="name">New Samsung TV</div>
+            <div className="name">New Iphone</div>
             <div className="description">It was time for a new TV</div>
           </div>
           <div className="right">
-            <div className="price">$500</div>
+            <div className="price red">-$700</div>
             <div className="datetime">2022-12-18 13:45</div>
           </div>
         </div>
